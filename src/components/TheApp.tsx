3f02df890d9f2e8df852f1c12eac0bc9de35b8ea
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-09 20:27:38 +1100

    Add style for App

--- src/components/TheApp.tsx
+++ src/components/TheApp.tsx
@@ -1,10 +1,11 @@
 import type { Component } from 'solid-js'
+import * as style from './TheApp.css'
 
 const App: Component = () => {
 	return (
-		<>
+		<div class={style.root}>
 			<h1>Hello world!!!!</h1>
-		</>
+		</div>
 	)
 }
 
