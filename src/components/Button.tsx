102205854b196d8e679dbaff2f06907d80e40483
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-10 09:35:38 +1100

    Add test harness for changing language in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,17 @@ import * as css from './Button.css'
 import { useI18n } from '@/components/I18n'
 
 const Button: ParentComponent = (props) => {
-	const [t] = useI18n()
+	const [t, setLanguage] = useI18n()
+
+	function setTranslationToTest() {
+		setLanguage('test')
+	}
 
 	return (
-		<button class={css.root}>
+		<button
+			class={css.root}
+			onClick={setTranslationToTest}
+		>
 			{t().hello}
 			{props.children}
 		</button>
