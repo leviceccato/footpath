f75b07a5f13abbe7e55912f7c8fc74e6882ef520
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-10 09:43:30 +1100

    Enforce type safety in regular translations

--- src/translations/test.ts
+++ src/translations/test.ts
@@ -1,6 +1,8 @@
-export default {
+import { createTranslation } from '@/scripts/i18n'
+
+export default createTranslation({
 	hello: 'TEST LANG',
 	test: (count: number): string => {
 		return ''
 	},
-}
+})
