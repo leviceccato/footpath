299de511ae32aed82a552afa208bf7b1cb6d9ad0
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-10 09:07:54 +1100

    Add translations to app

--- src/components/TheApp.css.ts
+++ src/components/TheApp.css.ts
@@ -3,3 +3,7 @@ import { style } from '@vanilla-extract/css'
 export const root = style({
 	padding: 10,
 })
+
+export const header = style({})
+
+export const logoContainer = style({})
--- src/components/TheApp.tsx
+++ src/components/TheApp.tsx
@@ -1,11 +1,42 @@
 import type { Component } from 'solid-js'
-import * as style from './TheApp.css'
+import * as css from './TheApp.css'
+import { lastSegmentFromPath } from '@/scripts/utils'
+
+import I18n from '@/components/I18n'
+import Button from '@/components/Button'
+
+// Import translations
+
+import { defaultTranslation } from '@/scripts/i18n'
+const translationModules = import.meta.glob([
+	'@/translations/*.ts',
+	'!**/_default.ts',
+])
+
+// Generate language name to import map
+
+const translations = Object.keys(translationModules).reduce((result, path) => {
+	const language = lastSegmentFromPath(path)
+
+	result[language] = translations[path]
+
+	return result
+}, {})
 
 const App: Component = () => {
 	return (
-		<div class={style.root}>
-			<h1>Hello world!!!!</h1>
-		</div>
+		<I18n
+			defaultTranslation={defaultTranslation}
+			translations={translations}
+		>
+			<div class={css.root}>
+				<header class={css.header}>
+					<div class={css.logoContainer}>
+						<Button>Hello</Button>
+					</div>
+				</header>
+			</div>
+		</I18n>
 	)
 }
 
