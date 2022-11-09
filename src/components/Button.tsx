d74d43481971a5bbc7660684bb326ec784daaf01
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-10 09:42:25 +1100

    Test language toggling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,19 @@ import * as css from './Button.css'
 import { useI18n } from '@/components/I18n'
 
 const Button: ParentComponent = (props) => {
-	const [t, setLanguage] = useI18n()
+	const [t, language] = useI18n()
 
-	function setTranslationToTest() {
-		setLanguage('test')
+	function toggleLanguage() {
+		if (language.get() === '_default') {
+			return language.set('test')
+		}
+		language.set('_default')
 	}
 
 	return (
 		<button
 			class={css.root}
-			onClick={setTranslationToTest}
+			onClick={toggleLanguage}
 		>
 			{t().hello}
 			{props.children}
