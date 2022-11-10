5c61e8a3457ecc1fc6b41f972d73fa27efe62b90
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-11 09:43:21 +1100

    Add WIP theme provider

---
+++ src/components/Theme.tsx
@@ -0,0 +1,33 @@
+import { createContext, createSignal, useContext, createEffect } from 'solid-js'
+import { readableColor, parseToHsl, toColorString } from 'polished'
+import type { RgbColor } from 'polished/lib/types/color'
+import type { ParentComponent } from 'solid-js'
+
+// Function used purely to get return type for context
+
+function createThemeContext(initialColour: RgbColor) {
+	const [_, setColour] = createSignal(initialColour)
+	return [setColour] as const
+}
+
+type ThemeContext = ReturnType<typeof createThemeContext>
+
+const context = createContext<ThemeContext>()
+
+export function useTheme() {
+	return useContext(context)
+}
+
+const Theme: ParentComponent<{ initialColour: RgbColor }> = (props) => {
+	const [colour, setColour] = createSignal(props.initialColour)
+
+	const colourString = () => toColorString(colour())
+	const readable = () => readableColor(colourString())
+	const colourHsl = () => parseToHsl(colourString())
+
+	return (
+		<context.Provider value={[setColour]}>{props.children}</context.Provider>
+	)
+}
+
+export default Theme
