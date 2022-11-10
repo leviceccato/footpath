d9787869f76d6a24e870cab8b0c34a65cba921b2
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-10 17:19:57 +1100

    Add default props test

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,11 @@
+import { setDefaultProps } from '@/scripts/solid-helpers'
 import type { ParentComponent } from 'solid-js'
 import * as css from './Button.css'
 import { useI18n } from '@/components/I18n'
 
-const Button: ParentComponent = (props) => {
+const Button: ParentComponent<{ test?: string }> = (props) => {
+	setDefaultProps(props, { test: 'asd' })
+
 	const [t, language] = useI18n()
 
 	function toggleLanguage() {
@@ -17,7 +20,7 @@ const Button: ParentComponent = (props) => {
 			class={css.root}
 			onClick={toggleLanguage}
 		>
-			Hello from {t().language}
+			Hello from {t().language} {props.test}
 		</button>
 	)
 }
