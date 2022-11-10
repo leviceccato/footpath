a314e001b0e7608c9a66d15527b74987c525173d
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-11 10:29:13 +1100

    Add main theme logic

---
+++ src/components/ProviderTheme.css.ts
@@ -0,0 +1,19 @@
+import { createVar } from '@vanilla-extract/css'
+
+export const bgColourHueVar = createVar()
+
+export const bgColourSaturationVar = createVar()
+
+//  Bg colour lightness vars
+
+export const bgColourLightness0Var = createVar()
+export const bgColourLightness1Var = createVar()
+export const bgColourLightness2Var = createVar()
+export const bgColourLightness3Var = createVar()
+export const bgColourLightness4Var = createVar()
+export const bgColourLightness5Var = createVar()
+export const bgColourLightness6Var = createVar()
+export const bgColourLightness7Var = createVar()
+export const bgColourLightness8Var = createVar()
+export const bgColourLightness9Var = createVar()
+export const bgColourLightness10Var = createVar()
--- src/components/ProviderTheme.tsx
+++ src/components/ProviderTheme.tsx
@@ -1,13 +1,16 @@
 import { createContext, createSignal, useContext, createEffect } from 'solid-js'
+import { assignInlineVars } from '@vanilla-extract/dynamic'
 import { readableColor, parseToHsl, toColorString } from 'polished'
 import type { RgbColor } from 'polished/lib/types/color'
 import type { ParentComponent } from 'solid-js'
+import * as css from './ProviderTheme.css'
 
 // Function used purely to get return type for context
 
-function createThemeContext(initialColour: RgbColor) {
-	const [_, setColour] = createSignal(initialColour)
-	return [setColour] as const
+function createThemeContext(initialBgColour: RgbColor) {
+	const [_, setBgColour] = createSignal(initialBgColour)
+	const vars = assignInlineVars({})
+	return [{ vars }, setBgColour] as const
 }
 
 type ThemeContext = ReturnType<typeof createThemeContext>
@@ -18,15 +21,46 @@ export function useTheme() {
 	return useContext(context)
 }
 
-const ProviderTheme: ParentComponent<{ initialColour: RgbColor }> = (props) => {
-	const [colour, setColour] = createSignal(props.initialColour)
+const ProviderTheme: ParentComponent<{ initialBgColour: RgbColor }> = (
+	props,
+) => {
+	const [bgColour, setBgColour] = createSignal(props.initialBgColour)
 
-	const colourString = () => toColorString(colour())
-	const readable = () => readableColor(colourString())
-	const colourHsl = () => parseToHsl(colourString())
+	const bgColourString = () => toColorString(bgColour())
+	const bgColourHsl = () => parseToHsl(bgColourString())
+
+	const readable = () => readableColor(bgColourString())
+	const isColourLight = () => readable() === '#FFF'
+	const lightnessDirection = () => (isColourLight() ? -1 : 1)
+
+	function createBgLightnessVar(value: number): string {
+		return String(bgColourHsl().lightness + value * lightnessDirection())
+	}
+
+	const theme = () => {
+		return {
+			vars: assignInlineVars({
+				[css.bgColourHueVar]: String(bgColourHsl().hue),
+				[css.bgColourSaturationVar]: String(bgColourHsl().saturation),
+				[css.bgColourLightness0Var]: createBgLightnessVar(0),
+				[css.bgColourLightness1Var]: createBgLightnessVar(10),
+				[css.bgColourLightness2Var]: createBgLightnessVar(20),
+				[css.bgColourLightness3Var]: createBgLightnessVar(30),
+				[css.bgColourLightness4Var]: createBgLightnessVar(40),
+				[css.bgColourLightness5Var]: createBgLightnessVar(50),
+				[css.bgColourLightness6Var]: createBgLightnessVar(60),
+				[css.bgColourLightness7Var]: createBgLightnessVar(70),
+				[css.bgColourLightness8Var]: createBgLightnessVar(80),
+				[css.bgColourLightness9Var]: createBgLightnessVar(90),
+				[css.bgColourLightness10Var]: createBgLightnessVar(100),
+			}),
+		}
+	}
 
 	return (
-		<context.Provider value={[setColour]}>{props.children}</context.Provider>
+		<context.Provider value={[theme(), setBgColour]}>
+			{props.children}
+		</context.Provider>
 	)
 }
 
