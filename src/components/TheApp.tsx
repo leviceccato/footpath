312bfdf367d09eec3d764e0219b6171ef14153b0
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-10 21:13:36 +1100

    Move I18n provider into main so that it can be used in App

--- src/components/TheApp.tsx
+++ src/components/TheApp.tsx
@@ -1,51 +1,31 @@
 import { createSignal } from 'solid-js'
+import { useI18n } from '@/components/I18n'
 import type { Component } from 'solid-js'
 import * as css from './TheApp.css'
-import { lastSegmentFromPath } from '@/scripts/utils'
 
-import I18n from '@/components/I18n'
 import Button from '@/components/Button'
 import LogoLacy from '@/components/LogoLacy'
-
-// Import translations
-
-import { defaultTranslation } from '@/scripts/i18n'
-const translationModules = import.meta.glob(
-	['@/translations/*.ts', '!**/_default.ts'],
-	{ import: 'default' },
-)
-
-// Generate language name to import map
-
-const translations = Object.keys(translationModules).reduce((result, path) => {
-	const language = lastSegmentFromPath(path)
-
-	result[language] = translationModules[path]
-
-	return result
-}, {})
+import VisuallyHidden from '@/components/VisuallyHidden'
 
 const TheApp: Component = () => {
+	const [t] = useI18n()
+
 	return (
-		<I18n
-			defaultTranslation={defaultTranslation}
-			translations={translations}
-		>
-			<div class={css.root}>
-				<header class={css.header}>
-					<div class={css.logoContainer}>
-						<Button
-							class={css.logoLink}
-							href="/"
-						>
-							<LogoLacy class={css.logo} />
-						</Button>
-					</div>
-					<div class={css.tabContainer}></div>
-					<div class={css.menuContainer}></div>
-				</header>
-			</div>
-		</I18n>
+		<div class={css.root}>
+			<header class={css.header}>
+				<div class={css.logoContainer}>
+					<Button
+						class={css.logoLink}
+						href="/"
+					>
+						<VisuallyHidden>{t().lacy}</VisuallyHidden>
+						<LogoLacy class={css.logo} />
+					</Button>
+				</div>
+				<div class={css.tabContainer}></div>
+				<div class={css.menuContainer}></div>
+			</header>
+		</div>
 	)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,37 @@ import 'modern-normalize/modern-normalize.css'
 import '@/base.css'
 
 import { render } from 'solid-js/web'
+import { lastSegmentFromPath } from '@/scripts/utils'
 
+import I18n from '@/components/I18n'
 import App from '@/components/TheApp'
 
-render(() => <App />, document.getElementById('root') as HTMLElement)
+// Import translations
+
+import { defaultTranslation } from '@/scripts/i18n'
+const translationModules = import.meta.glob(
+	['@/translations/*.ts', '!**/_default.ts'],
+	{ import: 'default' },
+)
+
+// Generate language name to import map
+
+const translations = Object.keys(translationModules).reduce((result, path) => {
+	const language = lastSegmentFromPath(path)
+
+	result[language] = translationModules[path]
+
+	return result
+}, {})
+
+render(
+	() => (
+		<I18n
+			defaultTranslation={defaultTranslation}
+			translations={translations}
+		>
+			<App />
+		</I18n>
+	),
+	document.getElementById('root') as HTMLElement,
+)
