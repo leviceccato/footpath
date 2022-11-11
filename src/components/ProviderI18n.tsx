b967264de7c8c61db34b4014d709ca4450f9cf50
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-11 19:22:58 +1100

    Remove unecessary typing info

--- src/components/ProviderI18n.tsx
+++ src/components/ProviderI18n.tsx
@@ -3,22 +3,16 @@ import { defaultTranslation } from '@/scripts/i18n'
 import type { ParentComponent } from 'solid-js'
 import type { Translation, Translations } from '@/scripts/i18n'
 
-// Function used purely to get return type for context
-
 function createI18nContext(
 	initialTranslation: Translation,
 	initialLanguage: string,
 ) {
 	const [translation] = createSignal(initialTranslation)
 	const [language, setLanguage] = createSignal(initialLanguage)
-	return [translation, { get: language, set: setLanguage }] as const
+	return [translation, { get: language, set: setLanguage }]
 }
 
-type I18nContext = ReturnType<typeof createI18nContext>
-
-const context = createContext<I18nContext>(
-	createI18nContext(defaultTranslation, '_default'),
-)
+const context = createContext(createI18nContext(defaultTranslation, '_default'))
 
 export function useI18n() {
 	return useContext(context)
