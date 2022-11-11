1305271bcc2300de4431aa44be0b37126e3c7fdc
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-11 13:26:18 +1100

    Add theme colours to app styles

--- src/components/TheApp.css.ts
+++ src/components/TheApp.css.ts
@@ -1,22 +1,27 @@
 import { style } from '@vanilla-extract/css'
+import * as themeCss from '@/components/ProviderTheme.css'
 
-export const root = style({})
+export const root = style({
+	backgroundColor: themeCss.colour10Var,
+	height: '100vh',
+})
 
 export const header = style({
 	display: 'flex',
 	height: 40,
-	borderBottom: '1px solid currentColor',
+	borderBottom: `1px solid ${themeCss.colour9Var}`,
 })
 
 export const logoContainer = style({
 	padding: 4,
 	display: 'flex',
-	borderRight: '1px solid currentColor',
+	borderRight: `1px solid ${themeCss.colour9Var}`,
 })
 
 export const logo = style({
 	width: 48,
 	maxWidth: 'none',
+	color: themeCss.colour2Var,
 	paddingTop: '2%',
 })
 
@@ -26,7 +31,7 @@ export const logoLink = style({
 	display: 'flex',
 	alignItems: 'center',
 	':hover': {
-		backgroundColor: '#DDDDDD',
+		backgroundColor: themeCss.colour9Var,
 	},
 })
 
