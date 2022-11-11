353acda2cdbe7ecd3993f1657a8214400c683342
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-11 13:26:31 +1100

    Add WIP text component styles and logic

--- src/components/Text.css.ts
+++ src/components/Text.css.ts
@@ -1,3 +1,9 @@
 import { style } from '@vanilla-extract/css'
 
-export const root = style({})
+export const root = style({
+	display: 'block',
+	fontSize: 'inherit',
+	fontWeight: 400,
+	color: 'var(--color, inherit)',
+	fontFamily: 'var(--font-interdisplay)',
+})
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,12 +3,29 @@ import { setDefaultProps } from '@/scripts/solid-helpers'
 import type { ParentComponent } from 'solid-js'
 import * as css from './Text.css'
 
-const Text: ParentComponent<{ tag?: string; class?: string }> = (props) => {
-	setDefaultProps(props, { tag: 'span', class: '' })
+const Text: ParentComponent<{
+	tag?: string
+	class?: string
+	variant:
+		| 'body-xsmall'
+		| 'body-small'
+		| 'body-medium'
+		| 'heading-small'
+		| 'heading-medium'
+		| 'heading-large'
+}> = (props) => {
+	setDefaultProps(props, { tag: 'span' })
+
+	const variant = () => {
+		const [style, size] = props.variant.split('-')
+		return { style, size }
+	}
 
 	return (
 		<Dynamic
-			classList={`${css.root} ${props.class}`}
+			classList={`${css.root} ${css[variant().style]} ${css[variant().size]} ${
+				props.class ?? ''
+			}`}
 			component={props.tag}
 		>
 			{props.children}
