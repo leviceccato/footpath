952bcf3b677d13ce93d6075fe170efb032b6354f
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-11 14:23:35 +1100

    Simplify function

--- src/components/ProviderTheme.tsx
+++ src/components/ProviderTheme.tsx
@@ -10,7 +10,7 @@ import * as css from './ProviderTheme.css'
 
 function createThemeContext() {
 	const [_, setColour] = createSignal<string>()
-	const [vars, __] = createSignal<ReturnType<typeof assignInlineVars>>()
+	const vars = () => assignInlineVars({})
 	return [vars, setColour] as const
 }
 
@@ -26,28 +26,24 @@ export function useTheme() {
 
 const ProviderTheme: ParentComponent<{ initialColour: string }> = (props) => {
 	const [colour, setColour] = createSignal(props.initialColour)
-	const [vars, setVars] = createSignal<ReturnType<typeof assignInlineVars>>()
 
 	const readable = () => readableColor(colour())
 
-	createEffect(() => {
-		setVars(
-			assignInlineVars({
-				[css.colourVar]: colour(),
-				[css.colour50Var]: mix(0.95, colour(), readable()),
-				[css.colour100Var]: mix(0.9, colour(), readable()),
-				[css.colour200Var]: mix(0.8, colour(), readable()),
-				[css.colour300Var]: mix(0.7, colour(), readable()),
-				[css.colour400Var]: mix(0.6, colour(), readable()),
-				[css.colour500Var]: mix(0.5, colour(), readable()),
-				[css.colour600Var]: mix(0.4, colour(), readable()),
-				[css.colour700Var]: mix(0.3, colour(), readable()),
-				[css.colour800Var]: mix(0.2, colour(), readable()),
-				[css.colour900Var]: mix(0.1, colour(), readable()),
-				[css.colour1000Var]: readable(),
-			}),
-		)
-	})
+	const vars = () =>
+		assignInlineVars({
+			[css.colourVar]: colour(),
+			[css.colour50Var]: mix(0.95, colour(), readable()),
+			[css.colour100Var]: mix(0.9, colour(), readable()),
+			[css.colour200Var]: mix(0.8, colour(), readable()),
+			[css.colour300Var]: mix(0.7, colour(), readable()),
+			[css.colour400Var]: mix(0.6, colour(), readable()),
+			[css.colour500Var]: mix(0.5, colour(), readable()),
+			[css.colour600Var]: mix(0.4, colour(), readable()),
+			[css.colour700Var]: mix(0.3, colour(), readable()),
+			[css.colour800Var]: mix(0.2, colour(), readable()),
+			[css.colour900Var]: mix(0.1, colour(), readable()),
+			[css.colour1000Var]: readable(),
+		})
 
 	return (
 		<context.Provider value={[vars, setColour]}>
