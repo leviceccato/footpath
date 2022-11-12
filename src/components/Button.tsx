3d38aaed981fda311dea27cce724a380ce97ecf0
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-12 21:07:17 +1100

    Allow passing text with implicit Text wrapper to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { Dynamic } from 'solid-js/web'
+import { Show } from 'solid-js'
 import type { ParentComponent, JSX } from 'solid-js'
 import * as css from './Button.css'
 
@@ -8,6 +9,7 @@ const Button: ParentComponent<
 	JSX.ButtonHTMLAttributes<HTMLButtonElement> & {
 		class?: string
 		href?: string
+		text?: string
 	}
 > = (props) => {
 	const tag = () => {
@@ -23,7 +25,12 @@ const Button: ParentComponent<
 			class={`${css.root} ${props.class ?? ''}`}
 			href={props.href}
 		>
-			<Text variant="bodyXs">{props.children}</Text>
+			<Show
+				when={props.text}
+				fallback={props.children}
+			>
+				<Text variant="bodyXs">{props.text}</Text>
+			</Show>
 		</Dynamic>
 	)
 }
