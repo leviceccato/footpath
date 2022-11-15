2a0a5862ee9fd461c4a4b9d34ac99ff837925e5e
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-15 11:48:35 +1100

    Add error display component

---
+++ src/components/ErrorDisplay.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+export const root = style({
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center',
+	gap: 20,
+	height: '100%',
+})
---
+++ src/components/ErrorDisplay.tsx
@@ -0,0 +1,22 @@
+import { ErrorBoundary } from 'solid-js/web'
+import type { ParentComponent } from 'solid-js'
+import * as css from './ErrorDisplay.css'
+
+import Text from '@/components/Text'
+
+const ErrorDisplay: ParentComponent = (props) => {
+	return (
+		<ErrorBoundary
+			fallback={(error) => (
+				<div class={css.root}>
+					<Text variant="bodyS">An error has occured in script execution:</Text>
+					<Text variant="bodyXs">{error}</Text>
+				</div>
+			)}
+		>
+			{props.children}
+		</ErrorBoundary>
+	)
+}
+
+export default ErrorDisplay
