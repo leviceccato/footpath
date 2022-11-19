561b5b41262e4f8531e038ee2ada968bc0bc2dc8
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-19 21:49:45 +1100

    Add createRandomColour utility

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -7,3 +7,7 @@ export function lastSegmentFromPath(path: string): string {
 export function decimalToPercentage(decimal: number): string {
 	return `${Math.round(decimal * 100)}%`
 }
+
+export function createRandomColour(): string {
+	return '#' + (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)
+}
