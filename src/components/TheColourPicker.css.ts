656c6554135cb3a3817ef935261dda3b9d916131
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-20 13:25:44 +1100

    Add some canvas styles and spectrum drawing

--- src/components/TheColourPicker.css.ts
+++ src/components/TheColourPicker.css.ts
@@ -1,13 +1,17 @@
 import { style } from '@vanilla-extract/css'
+import * as themeCss from '@/components/ProviderTheme.css'
 
 export const root = style({})
 
 export const canvasContainer = style({
 	position: 'relative',
+	border: `1px solid ${themeCss.colour1000Var}`,
 })
 
 export const canvas = style({
 	display: 'block',
+	aspectRatio: '1',
+	width: '100%',
 })
 
 export const colourSelector = style({})
--- src/components/TheColourPicker.tsx
+++ src/components/TheColourPicker.tsx
@@ -1,11 +1,56 @@
+import { mergeProps, createSignal, onMount } from 'solid-js'
 import type { Component } from 'solid-js'
+import { useTheme } from '@/components/ProviderTheme'
 import * as css from './TheColourPicker.css'
 
-const TheColourPicker: Component = (props) => {
+const TheColourPicker: Component<{ class?: string; canvasSize?: number }> = (
+	props,
+) => {
+	const _props = mergeProps({ canvasSize: 180 }, props)
+
 	let canvasRef: HTMLCanvasElement | undefined
 
+	const [theme] = useTheme()
+
+	const [hue, setHue] = createSignal(0)
+
+	function drawSpectrum() {
+		const context = canvasRef?.getContext('2d')
+		if (!context) return
+
+		// Create hue to white gradient
+
+		let gradientWhiteToHue = context.createLinearGradient(
+			0,
+			0,
+			context.canvas.width,
+			0,
+		)
+		gradientWhiteToHue.addColorStop(0, 'hsla(0 0% 100%)')
+		gradientWhiteToHue.addColorStop(1, `hsla(${hue()} 100% 50%)`)
+		context.fillStyle = gradientWhiteToHue
+		context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+
+		// Create transparent to black gradient
+
+		let gradientTransparentToBlack = context.createLinearGradient(
+			0,
+			0,
+			0,
+			context.canvas.height,
+		)
+		gradientTransparentToBlack.addColorStop(0, 'hsla(0 0% 0% / 0)')
+		gradientTransparentToBlack.addColorStop(1, 'hsla(0 0% 0% / 1)')
+		context.fillStyle = gradientTransparentToBlack
+		context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+	}
+
+	onMount(() => {
+		drawSpectrum()
+	})
+
 	return (
-		<div class={css.root}>
+		<div class={`${css.root} ${_props.class ?? ''}`}>
 			<div class={css.canvasContainer}>
 				<canvas
 					ref={canvasRef}
