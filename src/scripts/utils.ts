c3063c6c032a0fe679b2555d6a7fe92914dec7b9
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-30 21:55:22 +1100

    Space out for readability

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -9,6 +9,7 @@ type HsvColour = {
 export function lastSegmentFromPath(path: string): string {
 	const lastSegment = path.substring(path.lastIndexOf('/') + 1)
 	const lastSegmentWithoutExtension = lastSegment.split('.')[0]
+
 	return lastSegmentWithoutExtension
 }
 
