40f67e2594f33ce2062da5f056ea424fd7cdb800
Levi Ceccato <levi.ceccato@gmail.com>
2022-11-30 22:18:16 +1100

    Add popover and tooltip text to icon button

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,14 +3,32 @@ import { useIcons } from '@/components/ProviderIcons'
 import type { IconName } from '@/components/ProviderIcons'
 
 import Button from '@/components/Button'
+import Popover from '@/components/Popover'
+import Text from '@/components/Text'
 
-const IconButton: Component<{ class?: string; name: IconName }> = (props) => {
+const IconButton: Component<{
+	class?: string
+	name: IconName
+	tooltip: string
+}> = (props) => {
 	const [Icon] = useIcons()
 
 	return (
-		<Button class={props.class}>
-			<Icon name={props.name} />
-		</Button>
+		<Popover
+			class={props.class}
+			when="hover"
+			options={{
+				placement: 'bottom-end',
+				modifiers: [{ name: 'offset', options: { offset: [0, 9] } }],
+			}}
+			reference={() => (
+				<Button class={props.class}>
+					<Icon name={props.name} />
+				</Button>
+			)}
+		>
+			<Text variant="bodyXs">{props.tooltip}</Text>
+		</Popover>
 	)
 }
 
