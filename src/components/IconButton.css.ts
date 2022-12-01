b747326cf8a77496846bfe39562c1d39334a0fa3
Levi Ceccato <levi.ceccato@gmail.com>
2022-12-02 10:52:34 +1100

    Add tooltip styles

--- src/components/IconButton.css.ts
+++ src/components/IconButton.css.ts
@@ -1,6 +1,15 @@
 import { style } from '@vanilla-extract/css'
+import * as themeCss from '@/components/ProviderTheme.css'
 
 export const icon = style({
 	width: '100%',
 	height: '100%',
+})
+
+export const tooltip = style({
+	borderRadius: 4,
+	pointerEvents: 'none',
+	backgroundColor: themeCss.colourVar,
+	border: `1px solid ${themeCss.colour200Var}`,
+	boxShadow: ' 0px 10px 15px -3px rgba(0,0,0,0.1)',
 })
