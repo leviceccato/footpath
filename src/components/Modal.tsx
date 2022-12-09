24e0fff505042ae593e6863fd01ecc08594c96f4
Levi Ceccato <levi.ceccato@gmail.com>
2022-12-09 12:41:44 +1100

    Flesh out modal a bit more

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,41 @@
+import { Show, createSignal, createEffect } from 'solid-js'
 import type { ParentComponent } from 'solid-js'
 import { Portal } from 'solid-js/web'
 import { usePortal } from '@/components/ProviderPortal'
+import * as css from './Modal.css'
 
-const Modal: ParentComponent = (props) => {
+import ProviderFocusTrap from '@/components/ProviderFocusTrap'
+
+const Modal: ParentComponent<{
+	isShown: boolean
+	onShow: () => void
+	onHide: () => void
+}> = (props) => {
 	const [portal] = usePortal()
 
-	return <Portal mount={portal().get('modal')}>{props.children}</Portal>
+	const [isShown, setIsShown] = createSignal(props.isShown)
+
+	createEffect(() => {
+		setIsShown(props.isShown)
+	})
+
+	createEffect(() => {
+		if (isShown()) {
+			return props?.onShow()
+		}
+		props?.onHide()
+	})
+
+	return (
+		<Portal mount={portal().get('modal')}>
+			<Show when={isShown()}>
+				<ProviderFocusTrap when={isShown()}>
+					<div class={css.header}>Modal</div>
+					{props.children}
+				</ProviderFocusTrap>
+			</Show>
+		</Portal>
+	)
 }
 
 export default Modal
