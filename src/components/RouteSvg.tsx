73e1fc8663c0fee011a90949f67316060bd14ee0
Levi Ceccato <levi.ceccato@gmail.com>
2022-12-24 09:30:06 +1100

    Only show svg route if inside iframe

--- src/components/RouteSvg.tsx
+++ src/components/RouteSvg.tsx
@@ -1,7 +1,15 @@
 import type { Component } from 'solid-js'
+import { isInIframe } from '@/scripts/utils'
+import { useNavigate } from '@solidjs/router'
 import * as css from './RouteSvg.css'
 
 const RouteSvg: Component = () => {
+	const navigate = useNavigate()
+
+	if (!isInIframe()) {
+		navigate('/', { replace: true })
+	}
+
 	return <div class={css.root}></div>
 }
 
