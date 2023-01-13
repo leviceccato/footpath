58e3a4a9f969f4e83543554e2ca23872f8bbdb56
Levi Ceccato <levi.ceccato@gmail.com>
2023-01-13 12:08:31 +1100

    Add login modal

---
+++ src/components/ModalLogin.tsx
@@ -0,0 +1,17 @@
+import type { Component } from 'solid-js'
+import * as css from './ModalAbout.css'
+
+import Modal from '@/components/Modal'
+import type { ModalProps } from '@/components/Modal'
+
+const ModalLogin: Component<{
+	modal: ModalProps
+}> = (props) => {
+	return (
+		<Modal {...props.modal}>
+			<div class={css.root}></div>
+		</Modal>
+	)
+}
+
+export default ModalLogin
