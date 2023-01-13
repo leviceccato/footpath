6b94d9dbdf44321e2fb734e2da79d65e3bc726f5
Levi Ceccato <levi.ceccato@gmail.com>
2023-01-13 12:07:13 +1100

    Move provider setup into main app component

--- src/components/TheApp.tsx
+++ src/components/TheApp.tsx
@@ -1,24 +1,74 @@
-import { Routes, Route } from '@solidjs/router'
+import { Router, Routes, Route } from '@solidjs/router'
 import type { Component } from 'solid-js'
 import * as css from './TheApp.css'
+import { lastSegmentFromPath } from '@/scripts/utils'
+import { parseToHsl } from 'polished'
+import { colourBrand } from '@/data/colours'
+import type { Translation, Translations } from '@/scripts/i18n'
 
+import ProviderI18n from '@/components/ProviderI18n'
+import ProviderTheme from '@/components/ProviderTheme'
+import ProviderIcons from '@/components/ProviderIcons'
+import ProviderPortal from '@/components/ProviderPortal'
+
+// Import translations and generate languageName -> importFunc map
+
+const translationModules = import.meta.glob<Translation>(
+	['@/translations/*.ts', '!**/_default.ts'],
+	{ import: 'default' },
+)
+
+const translations = Object.keys(translationModules).reduce<Translations>(
+	(result, path) => {
+		const language = lastSegmentFromPath(path)
+		result[language] = translationModules[path]
+		return result
+	},
+	{},
+)
+
+// Initialise theme data
+
+const localStorageColour = localStorage.getItem('colour')
+const colour = localStorageColour ? parseToHsl(localStorageColour) : colourBrand
+const shouldUseSystem = localStorage.getItem('shouldUseSystem') || 'false'
+
+// Define portal mounts
+
+const mountIds = ['modal', 'tooltip']
 import RouteMain from '@/components/RouteMain'
 import RouteSvg from '@/components/RouteSvg'
 
 const TheApp: Component = () => {
 	return (
-		<div class={css.root}>
-			<Routes>
-				<Route
-					path="/"
-					component={RouteMain}
-				/>
-				<Route
-					path="/svg"
-					component={RouteSvg}
-				/>
-			</Routes>
-		</div>
+		<ProviderI18n
+			defaultLanguage="_default"
+			translations={translations}
+		>
+			<ProviderTheme
+				initialColour={colour}
+				initialShouldUseSystem={shouldUseSystem}
+			>
+				<ProviderPortal mountIds={mountIds}>
+					<ProviderIcons>
+						<div class={css.root}>
+							<Router>
+								<Routes>
+									<Route
+										path="/"
+										component={RouteMain}
+									/>
+									<Route
+										path="/svg"
+										component={RouteSvg}
+									/>
+								</Routes>
+							</Router>
+						</div>
+					</ProviderIcons>
+				</ProviderPortal>
+			</ProviderTheme>
+		</ProviderI18n>
 	)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,63 +5,16 @@ import 'modern-normalize/modern-normalize.css'
 import '@/base.css'
 
 import { render } from 'solid-js/web'
-import { Router } from '@solidjs/router'
-import { lastSegmentFromPath } from '@/scripts/utils'
-import { parseToHsl } from 'polished'
-import { colourBrand } from '@/data/colours'
-import type { Translation, Translations } from './scripts/i18n'
+import PocketBase from 'pocketbase'
 
-import ProviderI18n from '@/components/ProviderI18n'
-import ProviderTheme from '@/components/ProviderTheme'
-import ProviderIcons from '@/components/ProviderIcons'
-import ProviderPortal from '@/components/ProviderPortal'
 import TheApp from '@/components/TheApp'
 
-// Import translations and generate languageName -> importFunc map
+render(() => <TheApp />, document.getElementById('root') as HTMLElement)
 
-const translationModules = import.meta.glob<Translation>(
-	['@/translations/*.ts', '!**/_default.ts'],
-	{ import: 'default' },
-)
+const pb = new PocketBase(import.meta.env.VITE_API_URL)
 
-const translations = Object.keys(translationModules).reduce<Translations>(
-	(result, path) => {
-		const language = lastSegmentFromPath(path)
-		result[language] = translationModules[path]
-		return result
-	},
-	{},
-)
-
-// Initialise theme data
-
-const localStorageColour = localStorage.getItem('colour')
-const colour = localStorageColour ? parseToHsl(localStorageColour) : colourBrand
-const shouldUseSystem = localStorage.getItem('shouldUseSystem') || 'false'
-
-// Define portal mounts
-
-const mountIds = ['modal', 'tooltip']
-
-render(
-	() => (
-		<ProviderI18n
-			defaultLanguage="_default"
-			translations={translations}
-		>
-			<ProviderTheme
-				initialColour={colour}
-				initialShouldUseSystem={shouldUseSystem}
-			>
-				<ProviderPortal mountIds={mountIds}>
-					<ProviderIcons>
-						<Router>
-							<TheApp />
-						</Router>
-					</ProviderIcons>
-				</ProviderPortal>
-			</ProviderTheme>
-		</ProviderI18n>
-	),
-	document.getElementById('root') as HTMLElement,
-)
+pb.collection('users')
+	.getFullList(200, { sort: '-created' })
+	.then((data) => {
+		console.log('data', data)
+	})
