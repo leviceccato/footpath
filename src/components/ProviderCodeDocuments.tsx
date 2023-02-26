e66430939929439a4797ee75d6703d5fe992cc19
Levi Ceccato <levi.ceccato@gmail.com>
2023-02-26 18:18:58 +1100

    Update documents provider to use new client storage API

---
+++ src/components/ProviderCodeDocuments.tsx
@@ -0,0 +1,64 @@
+import { createContext, useContext } from 'solid-js'
+import type { ParentComponent } from 'solid-js'
+import { createClientStore } from '@/utils/storage'
+
+type CodeDocument = {
+	id: string
+	index: number
+	name: string
+	createdAt: Date
+	deletedAt: Date | null
+	content: string
+}
+
+function createCodeDocumentsContext() {
+	const [codeDocuments, setCodeDocuments] = createClientStore<
+		Record<string, CodeDocument>
+	>('code-documents', 1, {})
+
+	const codeDocumentCount = () => Object.keys(codeDocuments).length
+
+	async function createCodeDocument(
+		name: string,
+		index: number,
+	): Promise<string> {
+		const uuid = await import('uuid')
+		const id = uuid.v4()
+
+		const document = {
+			id,
+			name,
+			index,
+			createdAt: new Date(),
+			deletedAt: null,
+			content: '',
+		}
+
+		setCodeDocuments({
+			...codeDocuments,
+			[id]: document,
+		})
+		return id
+	}
+
+	return [
+		codeDocuments,
+		{ setCodeDocuments, createCodeDocument, codeDocumentCount },
+	] as const
+}
+
+const context = createContext(createCodeDocumentsContext())
+
+export function useDocuments() {
+	return useContext(context)
+}
+
+const ProviderCodeDocuments: ParentComponent = (props) => {
+	return (
+		<context.Provider value={createCodeDocumentsContext()}>
+			{props.children}
+		</context.Provider>
+	)
+}
+
+export default ProviderCodeDocuments
