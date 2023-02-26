87f4c349c77e11dd1209e1d771fd1a55ac284d16
Levi Ceccato <levi.ceccato@gmail.com>
2023-02-26 17:04:07 +1100

    Use web worker for storing data in idb

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,18 +1,8 @@
-import { openDB } from 'idb'
-import type { DBSchema } from 'idb'
+// import type { DBSchema } from 'idb'
 import { createStore } from 'solid-js/store'
 import type { StoreNode } from 'solid-js/store'
-import { sequence } from '@/utils/misc'
-
-const storeName = 'data'
-const keyName = 'key'
-
-type ClientStoreSchema<T> = DBSchema & {
-	[storeName]: {
-		key: string
-		value: T
-	}
-}
+import StorageWorker from '@/workers/storage?worker'
+import type { StorageRequest, StorageResponse } from '@/workers/storage'
 
 export function createClientStore<T extends StoreNode>(
 	name: string,
@@ -20,28 +10,39 @@ export function createClientStore<T extends StoreNode>(
 	initialValue: T,
 ) {
 	const [store, setStore] = createStore(initialValue)
-	const dbPromise = openDB<ClientStoreSchema<T>>(name, version, {
-		upgrade(db) {
-			db.createObjectStore(storeName)
-		},
-	}).then(async (db) => {
-		const data = await db.get(storeName, keyName)
-		if (data) {
-			setStore(data)
-		}
+	const worker = new StorageWorker()
 
-		return db
+	worker.onmessage = ({ data }: MessageEvent<StorageResponse>) => {
+		if (data.type === 'get') {
+			setStore(data.payload.data)
+		}
+	}
+
+	worker.onerror = (error) => {
+		console.error('Error in storage worker', error)
+	}
+
+	request(worker, {
+		type: 'init',
+		payload: {
+			name,
+			version,
+		},
 	})
 
-	let setPromises: Promise<string>[] = []
-
 	async function _setStore(to: T): Promise<void> {
-		const [db] = await Promise.all([dbPromise, sequence(setPromises)])
-		setPromises = []
-
 		setStore(to)
-		setPromises.push(db.put(storeName, to, keyName))
+		request(worker, {
+			type: 'set',
+			payload: {
+				data: to,
+			},
+		})
 	}
 
 	return [store, _setStore] as const
+}
+
+function request(worker: Worker, message: StorageRequest): void {
+	worker.postMessage(message)
 }
---
+++ src/workers/storage.ts
@@ -0,0 +1,103 @@
+import { openDB } from 'idb'
+import type { IDBPDatabase } from 'idb'
+
+type RequestInit = {
+	type: 'init'
+	payload: {
+		name: string
+		version: number
+	}
+}
+
+type RequestSet = {
+	type: 'set'
+	payload: {
+		data: any
+	}
+}
+
+type RequestGet = {
+	type: 'get'
+	payload: {}
+}
+
+export type StorageRequest = RequestInit | RequestGet | RequestSet
+
+type ResponseInit = {
+	type: 'init'
+	payload: {}
+}
+
+type ResponseGet = {
+	type: 'get'
+	payload: {
+		data: any
+	}
+}
+
+type ResponseSet = {
+	type: 'set'
+	payload: {}
+}
+
+export type StorageResponse = ResponseInit | ResponseGet | ResponseSet
+
+const storeName = 'store'
+const keyName = 'key'
+
+let dbPromise: Promise<IDBPDatabase<unknown>> | undefined
+
+self.onmessage = ({ data }: MessageEvent<StorageRequest>) => {
+	if (data.type === 'init') {
+		return init(data.payload.name, data.payload.version)
+	}
+	if (data.type === 'get') {
+		return get()
+	}
+	if (data.type === 'set') {
+		return set(data.payload.data)
+	}
+}
+
+function init(name: string, version: number): void {
+	dbPromise = openDB(name, version, {
+		upgrade(db) {
+			db.createObjectStore(storeName)
+		},
+	}).then((db) => {
+		respond({
+			type: 'init',
+			payload: {},
+		})
+		return db
+	})
+	get()
+}
+
+function get(): void {
+	dbPromise?.then(async (db) => {
+		const data = await db.get(storeName, keyName)
+
+		respond({
+			type: 'get',
+			payload: {
+				data,
+			},
+		})
+	})
+}
+
+function set(data: any): void {
+	dbPromise?.then(async (db) => {
+		await db.put(storeName, data, keyName)
+
+		respond({
+			type: 'set',
+			payload: {},
+		})
+	})
+}
+
+function respond(message: StorageResponse): void {
+	self.postMessage(message)
+}
