6153e32dd0c4e66cbe9063429a315a66b8b05cc4
Levi Ceccato <levi.ceccato@gmail.com>
2023-03-03 09:22:43 +1100

    Use rudimentary params for routes instead of solid router

--- src/components/TheApp.tsx
+++ src/components/TheApp.tsx
@@ -1,4 +1,4 @@
-import { Router, Routes, Route } from '@solidjs/router'
+import { Switch, Match } from 'solid-js'
 import type { Component } from 'solid-js'
 import * as css from './TheApp.css'
 import { lastSegmentFromPath } from '@/utils/misc'
@@ -11,7 +11,6 @@ import RouteMain from '@/components/RouteMain'
 import RouteSvg from '@/components/RouteSvg'
 
 // Import translations and generate languageName -> importFunc map
-
 const translationModules = import.meta.glob<Translation>(
 	['@/translations/*.ts', '!**/_default.ts'],
 	{ import: 'default' },
@@ -26,33 +25,33 @@ const translations = Object.keys(translationModules).reduce<Translations>(
 	{},
 )
 
+// Parse search params to figure out if SVG or main view should be shown
+const params = new URLSearchParams(window.location.search)
+const route = params.has('svg') ? 'svg' : 'main'
+
 const TheApp: Component = () => {
 	return (
-		<ProviderI18n
-			defaultLanguage="_default"
-			translations={translations}
-		>
-			<ProviderTheme>
-				<ProviderPortal mountIds={['modal', 'tooltip']}>
-					<ProviderIcons>
-						<div class={css.root}>
-							<Router>
-								<Routes>
-									<Route
-										path="/"
-										component={RouteMain}
-									/>
-									<Route
-										path="/svg"
-										component={RouteSvg}
-									/>
-								</Routes>
-							</Router>
-						</div>
-					</ProviderIcons>
-				</ProviderPortal>
-			</ProviderTheme>
-		</ProviderI18n>
+		<Switch>
+			<Match when={route === 'svg'}>
+				<RouteSvg />
+			</Match>
+			<Match when={route === 'main'}>
+				<ProviderI18n
+					defaultLanguage="_default"
+					translations={translations}
+				>
+					<ProviderTheme>
+						<ProviderPortal mountIds={['modal', 'tooltip']}>
+							<ProviderIcons>
+								<div class={css.root}>
+									<RouteMain />
+								</div>
+							</ProviderIcons>
+						</ProviderPortal>
+					</ProviderTheme>
+				</ProviderI18n>
+			</Match>
+		</Switch>
 	)
 }
 
