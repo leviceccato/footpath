323008f50a41a067476b511607a3b463bfe9bdb0
Levi Ceccato <levi.ceccato@gmail.com>
2023-03-03 21:54:44 +1100

    Use History API instead of Solid router

--- src/components/RouteSvg.tsx
+++ src/components/RouteSvg.tsx
@@ -1,13 +1,10 @@
 import type { Component } from 'solid-js'
 import { isInIframe } from '@/utils/misc'
-import { useNavigate } from '@solidjs/router'
 import * as css from './RouteSvg.css'
 
 const RouteSvg: Component = () => {
-	const navigate = useNavigate()
-
 	if (!isInIframe()) {
-		navigate('/', { replace: true })
+		window.history.replaceState(null, '', window.location.origin)
 	}
 
 	return <div class={css.root}></div>
