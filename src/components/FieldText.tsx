4c3f03ba237fec4f8d4d118679706c249910a00b
Levi Ceccato <levi.ceccato@gmail.com>
2023-04-16 20:06:19 +1000

    Add more WIP stuff

--- src/components/FieldText.tsx
+++ src/components/FieldText.tsx
@@ -1,18 +1,32 @@
-import type { ParentComponent } from 'solid-js'
+import { createSignal, Show } from 'solid-js'
+import type { ParentComponent, JSX } from 'solid-js'
+import type { ClassedComponent } from '@/utils/misc'
+import { Dynamic } from 'solid-js/web'
 
-type FieldTextProps = {
+type FieldTextProps = ClassedComponent & {
+	value: string
+	label: JSX.Element
+	placeholder?: string
+	isDisabled?: boolean
 	rows?: number
 	maxRows?: number
 	isResizable?: boolean
 }
 
-const FieldText: ParentComponent<FieldTextProps> = (_) => {
+const FieldText: ParentComponent<FieldTextProps> = (props) => {
+	const [isFocused, setIsFocused] = createSignal(false)
+
 	return (
-		<div>
-			<label>
-				<input />
-			</label>
-		</div>
+		<label class={props.class ?? ''}>
+			<Show when={props.label}>
+				<div>{props.label}</div>
+			</Show>
+			<Dynamic
+				component={'input'}
+				onfocus={[setIsFocused, true]}
+				onblur={[setIsFocused, false]}
+			/>
+		</label>
 	)
 }
 
