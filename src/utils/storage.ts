d4ef4f74ebac3a4cd93e046dc15f9af3ea832c13
Levi Ceccato <levi.ceccato@gmail.com>
2023-09-13 09:16:14 +1000

    Use defaults util for optional options

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,19 +1,22 @@
 import { createSignal, createEffect, createRoot } from 'solid-js'
 import StorageWorker from '@/utils/storage.worker?worker'
 import { type StorageRequest, StorageResponse } from '@/utils/storage.worker'
+import { defaults } from '@/utils/misc'
 
 type CreateClientStoreOptions<TValue> = {
 	name: string
 	version: number
 	initialValue: TValue
-	onError?: (error: ErrorEvent) => void
+	onError?: ((error: ErrorEvent) => void) | null
 	shouldPersist?: boolean
 }
 
 export function createClientStore<TValue>(
-	options: CreateClientStoreOptions<TValue>,
+	rawOptions: CreateClientStoreOptions<TValue>,
 ) {
 	return createRoot(() => {
+		const options = defaults(rawOptions, { shouldPersist: true, onError: null })
+
 		const [store, setStore] = createSignal({ value: options.initialValue })
 
 		// Worker is created in promise so it can resolve asynchronously while
@@ -22,6 +25,8 @@ export function createClientStore<TValue>(
 
 		const workerPromise = new Promise<Worker>((resolve) => {
 			const worker = new StorageWorker()
+
+			worker.onerror = options.onError
 
 			worker.onmessage = ({ data }: MessageEvent<StorageResponse>) => {
 				switch (data.type) {
@@ -34,8 +39,6 @@ export function createClientStore<TValue>(
 				}
 			}
 
-			worker.onerror = options.onError ?? null
-
 			request(worker, {
 				type: 'init',
 				payload: {
@@ -45,7 +48,7 @@ export function createClientStore<TValue>(
 			})
 		})
 
-		if (options.shouldPersist ?? true) {
+		if (options.shouldPersist) {
 			createEffect(() => {
 				const data = store().value || options.initialValue
 
