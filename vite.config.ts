aa370450c31f979977ce1c94e21d4059a36550e8
Levi Ceccato <levi.ceccato@gmail.com>
2023-12-10 16:01:30 +1100

    Remove failing rules

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import solidPlugin from 'vite-plugin-solid'
 
 const src = new URL('./src', import.meta.url)
 
-const a = 2
-const b = 2
-
 export default {
 	clearScreen: false,
 	publicDir: 'src/static',
