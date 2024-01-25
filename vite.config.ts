1c05c9430484e18144b6725779f06310db7fac24
Levi Ceccato <levi.ceccato@gmail.com>
2024-01-25 13:31:32 +1100

    Nest index html

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import { type UserConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 const src = new URL('./src', import.meta.url)
+const dist = new URL('./dist', import.meta.url)
 
 export default {
+	root: src.pathname,
 	clearScreen: false,
-	publicDir: 'src/static',
+	publicDir: 'static',
 	plugins: [solidPlugin(), vanillaExtractPlugin()],
 	server: {
 		host: true,
+	},
+	build: {
+		outDir: dist.pathname,
+		emptyOutDir: true,
 	},
 	resolve: {
 		alias: {
