a07f0f8f603ef5a08174686ddf3859464c44d741
Levi Ceccato <levi.ceccato@gmail.com>
2024-01-29 16:41:59 +1100

    Add floating component styles

---
+++ src/components/Floating.css.ts
@@ -0,0 +1,33 @@
+import * as themeCss from '@/providers/Theme.css'
+import { style, styleVariants } from '@vanilla-extract/css'
+
+export const content = style({})
+
+export const contentVariants = styleVariants({
+	shown: [
+		content,
+		{
+			display: 'block',
+		},
+	],
+	hidden: [
+		content,
+		{
+			display: 'none',
+		},
+	],
+})
+
+export const arrow = style({
+	position: 'absolute',
+	top: -3.5,
+})
+
+export const arrowInner = style({
+	width: 7,
+	height: 7,
+	backgroundColor: themeCss.colourVar,
+	borderTop: `1px solid ${themeCss.colour200Var}`,
+	borderRight: `1px solid ${themeCss.colour200Var}`,
+	transform: 'rotate(-45deg)',
+})
