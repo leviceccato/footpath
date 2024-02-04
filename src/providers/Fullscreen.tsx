f8ff612c005032189256a220207a75c39dcc5c1c
Levi Ceccato <levi.ceccato@gmail.com>
2024-02-04 21:04:01 +1100

    Ensure reactivity works

--- src/providers/Fullscreen.tsx
+++ src/providers/Fullscreen.tsx
@@ -1,6 +1,7 @@
 import {
 	type ParentComponent,
 	createContext,
+	createRoot,
 	createSignal,
 	onCleanup,
 	onMount,
@@ -8,56 +9,58 @@ import {
 } from 'solid-js'
 
 function createFullscreenContext() {
-	const [isEnabled, setIsEnabled] = createSignal(false)
-	const [element, setElement] = createSignal<Element | null>(null)
+	return createRoot(() => {
+		const [isEnabled, setIsEnabled] = createSignal(false)
+		const [element, setElement] = createSignal<Element | null>(null)
 
-	function handleChange(): void {
-		setElement(document.fullscreenElement)
+		function handleChange(): void {
+			setElement(document.fullscreenElement)
 
-		/* If fullscreen was disabled for whatever reason, re-enable it */
-		if (document.fullscreenElement) {
-			setIsEnabled(true)
-		}
-	}
-
-	function handleError(): void {
-		/* Assume any error indicates that fullscreen is disabled */
-		setIsEnabled(false)
-	}
-
-	async function toggle(requestedElement: Element): Promise<Error | null> {
-		if (element()) {
-			try {
-				await document.exitFullscreen()
-				return null
-			} catch (cause) {
-				return new Error('Failed to exit fullscreen', { cause })
+			/* If fullscreen was disabled for whatever reason, re-enable it */
+			if (document.fullscreenElement) {
+				setIsEnabled(true)
 			}
 		}
-		try {
-			await requestedElement.requestFullscreen()
-			return null
-		} catch (cause) {
-			return new Error('Failed to enter fullscreen', { cause })
+
+		function handleError(): void {
+			/* Assume any error indicates that fullscreen is disabled */
+			setIsEnabled(false)
 		}
-	}
 
-	onMount(() => {
-		setIsEnabled(document.fullscreenEnabled)
-		document.addEventListener('fullscreenchange', handleChange)
-		document.addEventListener('fullscreenerror', handleError)
+		async function toggle(requestedElement: Element): Promise<Error | null> {
+			if (element()) {
+				try {
+					await document.exitFullscreen()
+					return null
+				} catch (cause) {
+					return new Error('Failed to exit fullscreen', { cause })
+				}
+			}
+			try {
+				await requestedElement.requestFullscreen()
+				return null
+			} catch (cause) {
+				return new Error('Failed to enter fullscreen', { cause })
+			}
+		}
+
+		onMount(() => {
+			setIsEnabled(document.fullscreenEnabled)
+			document.addEventListener('fullscreenchange', handleChange)
+			document.addEventListener('fullscreenerror', handleError)
+		})
+
+		onCleanup(() => {
+			document.removeEventListener('fullscreenchange', handleChange)
+			document.removeEventListener('fullscreenerror', handleError)
+		})
+
+		return {
+			isEnabled,
+			element,
+			toggle,
+		}
 	})
-
-	onCleanup(() => {
-		document.removeEventListener('fullscreenchange', handleChange)
-		document.removeEventListener('fullscreenerror', handleError)
-	})
-
-	return {
-		isEnabled,
-		element,
-		toggle,
-	}
 }
 
 const context = createContext(createFullscreenContext())
