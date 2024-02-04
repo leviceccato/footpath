67257faacff8c2722aa9b76f3212e76c9ff0f992
Levi Ceccato <levi.ceccato@gmail.com>
2024-02-04 22:44:23 +1100

    Add WIP menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,23 @@
+import { Popover, type PopoverProps } from '@/components/Popover'
+import { type Component, type JSX } from 'solid-js'
+import * as css from './Menu.css'
+
+type Controller = {
+	isShown: boolean
+}
+
+export const Menu: Component<{
+	elementRef: PopoverProps['elementRef']
+	state: PopoverProps['state']
+	children: (_: Controller) => JSX.Element
+}> = (props) => {
+	const controller: Controller = {
+		isShown: false,
+	}
+
+	return (
+		<Popover state={props.state} elementRef={props.elementRef} hasArrow={true}>
+			<div class={css.root}>{props.children(controller)}</div>
+		</Popover>
+	)
+}
