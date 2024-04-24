6f9d5d97d8e88f58e4e5c60dff156a4f0e677840
Levi Ceccato <levi.ceccato@gmail.com>
2024-04-24 19:52:12 +1000

    Add pointless error handling

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import { App } from '@/components/App'
 import 'modern-normalize/modern-normalize.css'
 import { render } from 'solid-js/web'
 
-const root = document.getElementById('root')
-if (root) {
+const rootId = 'root'
+const root = document.getElementById(rootId)
+if (!root) {
+	console.error(`Failed to mount app, no element with id '${rootId}' found`)
+} else {
 	render(() => <App />, root)
 }
